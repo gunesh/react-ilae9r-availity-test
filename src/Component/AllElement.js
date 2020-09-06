f4efbd15ea36b05fd967dad6b3307db4a0b94fb9
gunesh <gunesh26@gmail.com>
2020-09-06 20:20:38 +0530

    1 file modified

--- src/Component/AllElement.js
+++ src/Component/AllElement.js
@@ -54,6 +54,7 @@ const AllElement = prop => {
               initialValues={toJS(initialval)}
               onSubmit={form => {
                 console.log(form);
+                //myStore.submitMyForm(form)
               }}
               validationSchema={schema}
             >
