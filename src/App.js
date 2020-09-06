05af05d59cbe18d629bd1e8311ac7ba20a6e80ef
gunesh <gunesh26@gmail.com>
2020-09-06 16:44:38 +0530

    2 files modified

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from "react";
 import "./style.css";
 import FieldExample from './Component/FieldExample'
+import CheckboxExample from './Component/CheckboxExample'
 
 export default function App() {
   return (
     <>
-    Test
-    <FieldExample />
+    
+    <CheckboxExample />
     </>
   );
 }
